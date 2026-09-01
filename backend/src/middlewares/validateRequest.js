b8beb49d726ffa82c22e40a1a03d08684686bf47
Alname94 <alname94@gmail.com>
2026-09-01 16:06:17 -0300

    refactor: modify validateRequest for req.query case

--- backend/src/middlewares/validateRequest.js
+++ backend/src/middlewares/validateRequest.js
@@ -7,8 +7,12 @@ export const validateRequest = (schema) => async (req, res, next) => {
     });
     
     req.body = parsed.body;
-    req.query = parsed.query;
     req.params = parsed.params;
+
+    if (parsed.query) {
+      Object.keys(req.query).forEach((key) => delete req.query[key]);
+      Object.assign(req.query, parsed.query);
+    }
 
     next();
   } catch (error) {
