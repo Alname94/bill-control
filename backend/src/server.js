b1e46be6c63c568904859537685b1159437caf2e
Alname94 <alname94@gmail.com>
2026-09-12 14:23:57 -0300

    refactor: add db check asynchronously before start the server

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,18 +3,22 @@ import pool from "./config/db.js";
 
 const PORT = process.env.PORT || 3000;
 
-// Consulta de prueba para verificar la conexión a la base de datos
-pool.query("SELECT NOW()", (err, res) => {
-  if (err) {
-    console.error("Error al conectar con la base de datos en Neon:", err.stack);
-  } else {
+const startServer = async () => {
+  try {
+    const res = await pool.query("SELECT NOW()");
     console.log(
-      "Conexión exitosa a PostgreSQL en Neon. Hora en BD:",
-      res.rows[0].now,
+      "Conexión exitosa a la BD. Hora en BD:", res.rows[0].now,
     );
-  }
-});
 
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
-});
+    app.listen(PORT, () => {
+      console.log(`Servidor escuchando en http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error(
+      "Error al conectar con la base de datos:", err.stack,
+    );
+    process.exit(1);
+  }
+};
+
+startServer();
